400d832a6617dc460a212d5ea6e1687173ab7b6d
Francisco Reyes <francisco@makingdevs.com>
2016-04-11 14:58:41 -0500

    Tipo de dato numero entero

---
+++ numberInteger.rb
@@ -0,0 +1,7 @@
+puts "Numero entero #{2016}"
+puts "Numero entero guion bajo #{2_016}"
+puts "Numero negativo #{-2016}"
+puts "Numero octal #{3740}"
+puts "Numero hexadecimal #{0x7E0} "
+puts "Numero binario #{0b011111100000}"
+
