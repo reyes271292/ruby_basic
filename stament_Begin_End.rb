97b72df9be1648c4f4d31ccf066daf2061d6577b
Francisco Reyes <francisco@makingdevs.com>
2016-04-11 14:41:44 -0500

    Sentencia BEGIN, END

---
+++ stament_Begin_End.rb
@@ -0,0 +1,9 @@
+BEGIN{
+  puts "Inicio del programa #{Time.now.strftime "%d-%m-%Y %H:%M:%S "}"
+}
+
+END{
+  puts "Fin del programa #{Time.now.strftime "%d-%m-%Y %H:%M:%S "}"
+}
+
+puts "Hola mundo"
