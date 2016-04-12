66abca67f59954cf48ebe6c78253059874ece87d
Francisco Reyes <francisco@makingdevs.com>
2016-04-12 18:58:59 -0500

    Operadores de asignacion

---
+++ operatorsAssigment.rb
@@ -0,0 +1,10 @@
+puts "\tOperadores de asignacion"
+valorA=10
+nueva=0
+puts "Asignacion simple nueva= "+(nueva=valorA).to_s
+puts "Suma y asigna nueva+= "+(nueva+=valorA).to_s
+puts "Resta y asigna nueva+= "+(nueva-=valorA).to_s
+puts "Multiplica y asigna nueva*= "+(nueva*=valorA).to_s
+puts "Divide y asigna nueva/= "+(nueva/=valorA).to_s
+puts "Exponente y asigna nueva**= "+(nueva**=valorA).to_s
+puts "Modulo y asigna nueva%= "+(nueva%=valorA).to_s
