d6f800975e6643b2500fb3863a2aaebc63d9cded
Francisco Reyes <francisco@makingdevs.com>
2016-04-12 18:46:50 -0500

    Operadores de comparacion

---
+++ comparisonOperators.rb
@@ -0,0 +1,8 @@
+puts "\tOperadores de comparacion"
+puts "Igual 1==1 "+(1==1).to_s
+puts "Diferente 1!=2 "+(1!=2).to_s
+puts "Mayor 1>2 "+(1>2).to_s
+puts "Menor 1<2 "+(1<2).to_s
+puts "Mayor o igual 1>=2 "+(1>=2).to_s
+puts "Spaceship 1<=>2 "+(1<=>2).to_s
+puts "Compara valor y tipo 1 === Cadena "+(1==="Cadena").to_s
