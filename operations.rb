5693456236f8c5de45822389c56655ded6f10b56
Francisco Reyes <francisco@makingdevs.com>
2016-04-12 18:30:21 -0500

    Operaciones basicas

---
+++ operations.rb
@@ -0,0 +1,7 @@
+puts "\tOperaciones basicas"
+puts "Suma "+(10+5).to_s
+puts "Resta "+(10-5).to_s
+puts "Multiplicacion "+(10*5).to_s
+puts "Division "+(10/5).to_s
+puts "Modulo "+(10%5).to_s
+puts "Potencia "+(10**5).to_s
