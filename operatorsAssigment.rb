dfe55e4e5ed402901c37b0eb86935a461a064adb
Francisco Reyes <francisco@makingdevs.com>
2016-04-12 19:02:32 -0500

    Operadores de asignacion

--- operatorsAssigment.rb
+++ operatorsAssigment.rb
@@ -1,6 +1,11 @@
-puts "\tOperadores de asignacion"
-valorA=10
-nueva=0
+valorA,valorB,nueva = 10,20,0
+
+puts "\t Asignacion multiple"
+puts "valorA = #{valorA}"
+puts "valorB = #{valorB}"
+puts "nueva = #{nueva}"
+
+puts "\t\vOperadores de asignacion"
 puts "Asignacion simple nueva= "+(nueva=valorA).to_s
 puts "Suma y asigna nueva+= "+(nueva+=valorA).to_s
 puts "Resta y asigna nueva+= "+(nueva-=valorA).to_s
